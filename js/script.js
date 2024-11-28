5d735eded87bac3a57ad41b49ea1d9bf963bda96
ElfboyGabi <gabitheelf.art@gmail.com>
2024-11-28 18:46:08 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ function fadeOutLoadingScreen() {
 
         // Redirect to another page after a delay
         setTimeout(() => {
-            window.location.href = '../!FINALS/html/login.html'; // Change this to your target URL
+            window.location.href = '/html/login.html'; // Change this to your target URL
         }, 2000); // Wait for 2 seconds before redirecting (adjust as necessary)
     }, 1000); // Wait for fade-out transition to finish
 }
